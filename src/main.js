76e47b8900766203914164b3b1383ca760bbca74
Dam Nguyen <damnguyen.tga@gmail.com>
2024-03-04 16:47:44 +0700

    commit code

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "@fortawesome/fontawesome-free";
+import "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/free-brands-svg-icons";
+import "@fortawesome/free-regular-svg-icons";
+import "@fortawesome/free-solid-svg-icons";
